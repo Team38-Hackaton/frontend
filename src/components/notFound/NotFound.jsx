2375925b8e16f4bc4076ba8763b1b15011b22de0
Bella Bzhukova <104579797+bellabzhu@users.noreply.github.com>
2023-11-11 22:45:38 +0400

    добавила страницу 404

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,8 +1,12 @@
 import styles from './NotFound.module.css';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return(
-    <div className={styles.container}></div>
+    <div className={styles.container}>
+      <h1>Здесь ничего нет! 🥺</h1>
+      <Link to='/'>Вернуться на главную</Link>
+    </div>
   );
 };
 
