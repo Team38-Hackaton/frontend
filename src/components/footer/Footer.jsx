a1e37a6a7409508ed6d9c0d776fac49b3ded325a
Lirmortoch <dima2111323@gmail.com>
2023-11-11 15:07:16 +0300

    Добавил ссылки на профили. Немного изменил стили для ссылок

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,18 +3,20 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Это футер.
       <p className={styles.header}>Memory card game by Team38</p>
       <p className={styles.item}>Ссылка на репозиторий проекта: <Link className={styles.link} to=''>Memory game</Link></p>
      
       <div className={styles.credits}>
-        <p className={styles.header}>Ссылки на репозитории членов команды</p>
+        <p className={styles.credits_header}>Ссылки на репозитории членов команды</p>
 
         <div className={styles.links}>
-          <Link className={styles.link} to=''></Link>
+          <Link className={styles.link} to='https://github.com/bellabzhu'>Bella</Link>
+          <Link className={styles.link} to='https://github.com/AndersGrunge'>Andrei</Link>
+          <Link className={styles.link} to='https://github.com/Lirmortoch'>Lirmortoch</Link>
         </div>
       </div>
 
+      <p className={styles.item}>Все картинки взяты с Freepic</p>
       <p className={styles.item}>Copyright &copy;&nbsp;{new Date().getFullYear()}</p>
     </footer>
   );
