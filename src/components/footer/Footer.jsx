8069dfac3a4028c1f2368014819ed6566812ec5f
Lirmortoch <dima2111323@gmail.com>
2023-11-11 14:31:29 +0300

    Добавил информацию о проекте и комманде

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,11 +4,18 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       Это футер.
-      <p className={styles.header}>Игра от команды 38</p>
-      <hr className={styles.line} />
+      <p className={styles.header}>Memory card game by Team38</p>
+      <p className={styles.item}>Ссылка на репозиторий проекта: <Link className={styles.link} to=''>Memory game</Link></p>
+     
       <div className={styles.credits}>
-        <p className={styles.item}>&copy;&nbsp;{new Date().getFullYear()}</p>
+        <p className={styles.header}>Ссылки на репозитории членов команды</p>
+
+        <div className={styles.links}>
+          <Link className={styles.link} to=''></Link>
+        </div>
       </div>
+
+      <p className={styles.item}>Copyright &copy;&nbsp;{new Date().getFullYear()}</p>
     </footer>
   );
 };
